c1e357740c3d2a466d63e304c3ca21a8e4684822
Denis Ivanchenko <sicknature24@yandex.com>
2023-01-16 01:44:35 +0300

    feat: implement burger menu, add 380px styles for welcome, about sections

--- plants/index.js
+++ plants/index.js
@@ -1,2 +1,21 @@
 console.log(" 1. Вёрстка валидная +10 \n 2. Вёрстка семантическая +20 \n 3. Вёрстка соответствует макету +48 " +
     "\n 4. Требования к css +12 \n 5. Интерактивность, реализуемая через css +20 \n Итоговая оценка: 100 баллов")
+
+
+    /* Burger handler */
+
+    (function () {
+        const burgerItem = document.querySelector('.burger');
+        const menu = document.querySelector('.nav');
+        const closeMenu = document.querySelector('.nav-close');
+        const main = document.querySelector('.main');
+        burgerItem.addEventListener('click', () => {
+            menu.classList.add('nav-active');
+        });
+        closeMenu.addEventListener('click', () => {
+            menu.classList.remove('nav-active');
+        });
+        main.addEventListener('click', () => {
+            menu.classList.remove('nav-active');
+        });
+    }())
