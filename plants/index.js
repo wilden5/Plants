0c4200630c4ae71e37bced00f3a7086381927046
Denis Ivanchenko <sicknature24@yandex.com>
2023-01-17 02:38:37 +0300

    fix: update constants naming

--- plants/index.js
+++ plants/index.js
@@ -1,21 +1,25 @@
-console.log(" 1. Вёрстка валидная +10 \n 2. Вёрстка семантическая +20 \n 3. Вёрстка соответствует макету +48 " +
-    "\n 4. Требования к css +12 \n 5. Интерактивность, реализуемая через css +20 \n Итоговая оценка: 100 баллов")
+console.log(" 1. Вёрстка соответствует макету. Ширина экрана 768px +24" +
+    "\n 2. Вёрстка соответствует макету. Ширина экрана 380px +24" +
+    "\n 3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. " +
+    "\n 4. На ширине экрана 380рх и меньше реализовано адаптивное меню +22 \n Итоговая оценка: 75 баллов");
 
 
-    /* Burger handler */
+/* Burger handler starts */
 
-    (function () {
-        const burgerItem = document.querySelector('.burger');
-        const menu = document.querySelector('.nav');
-        const closeMenu = document.querySelector('.nav-close');
-        const main = document.querySelector('.main');
-        burgerItem.addEventListener('click', () => {
-            menu.classList.add('nav-active');
-        });
-        closeMenu.addEventListener('click', () => {
-            menu.classList.remove('nav-active');
-        });
-        main.addEventListener('click', () => {
-            menu.classList.remove('nav-active');
-        });
-    }())
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const navMenu = document.querySelector('.nav');
+    const closeButton = document.querySelector('.nav-close');
+    const main = document.querySelector('.main');
+    burgerItem.addEventListener('click', () => {
+        navMenu.classList.add('nav-active');
+    });
+    closeButton.addEventListener('click', () => {
+        navMenu.classList.remove('nav-active');
+    });
+    main.addEventListener('click', () => {
+        navMenu.classList.remove('nav-active');
+    });
+}())
+
+/* Burger handler ends */
